deeaf07f64a45ca2382d162a7fc99c4d605835e8
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-06-10 17:38:26 +0700

    melakukan perulangan setiap baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -30,6 +30,10 @@ int main(){
     // jika file ada maka
     if (infile.is_open())
     {
-        
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            
+        }
     }
 }
