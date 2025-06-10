a67642f7545da45d606bed44caffdc200dbbbe6b
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-06-10 18:35:01 +0700

    int main ()

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -4,3 +4,11 @@
 #include <array>
 //untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1...
+    try {
+        array<int, 3> data = { 10, 20, 30 };
+    }
+}
