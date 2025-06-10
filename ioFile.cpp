4dc8bdf1b4e5762e7c34ebc61361647e6bd7910d
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-06-10 17:31:24 +0700

    mendapatkan setiap karakter dalam satu baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -7,4 +7,13 @@ int main(){
     string baris;
     // membuka file dalam mode menulis. :
     ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open
+
+
+    while (true) {
+        cout << "_ ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+    }
 }
